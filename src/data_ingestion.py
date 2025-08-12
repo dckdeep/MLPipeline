2f02d8a191cb771ba45bbb8bb76b2bc908d8cecb
dckdeep <deepkandpal17@gmail.com>
2025-08-13 00:40:39 +0530

    Added code of data ingestion, preprocessing, feature enfgineering, model building, model evaluation with final output

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -88,8 +88,8 @@ def main():
     try:
         params = load_params(params_path='params.yaml')
         test_size = params['data_ingestion']['test_size']
-        test_size = 0.2
-        data_path = 'https://raw.githubusercontent.com/dckdeep/MLPipeline/refs/heads/main/experiments/spam.csv'
+        #test_size = 0.2
+        data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/main/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=2)
